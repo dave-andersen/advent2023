e492c18bca839e913d5ef06db5749b066fc6e2cc
Dave Andersen <dga@cs.cmu.edu>
2023-12-01 15:34:22 -0500

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::BufReader;
 
 const BASEDIR: &str = "/Users/dga/rust/advent2023";
@@ -16,33 +16,35 @@ fn open_buffered(filename: &str) -> BufReader<File> {
 
 fn a1(part_two: bool) {
     let input = input_file("a1.1");
-    let mut patterns = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    let values = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut patterns = vec![
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine",
+    ];
+    let values = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     if !part_two {
         patterns.truncate(10);
     }
 
     let ac = aho_corasick::AhoCorasick::new(patterns).unwrap();
-   
+
     let tot: u32 = open_buffered(&input)
         .lines()
         .map_while(Result::ok)
         .map(|line| {
-        let digits: Vec<u32> = ac.find_overlapping_iter(&line)
-            .map(|pid| values[pid.pattern().as_usize()]).collect();
-        if digits.is_empty() {
-            0
-        } else {
-            digits[0]*10 + digits[digits.len()-1]
-        }
-    }).sum();
+            let digits: Vec<u32> = ac
+                .find_overlapping_iter(&line)
+                .map(|pid| values[pid.pattern().as_usize()])
+                .collect();
+            if digits.is_empty() {
+                0
+            } else {
+                digits[0] * 10 + digits[digits.len() - 1]
+            }
+        })
+        .sum();
     println!("Total: {tot}");
 }
-
-
 
 fn main() {
     a1(false);
