5ce8fafa11dcc7b0c7ec6f8b9619280d52a42c17
Dave Andersen <dga@cs.cmu.edu>
2023-12-02 09:35:58 -0500

    Add solution for day 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use regex::Regex;
 
 const BASEDIR: &str = "/Users/dga/rust/advent2023";
 const INPUTDIR: &str = "inputs";
@@ -13,6 +14,14 @@ fn open_buffered(filename: &str) -> BufReader<File> {
     BufReader::new(File::open(filename).unwrap())
 }
 
+fn read_all_lines(filename: &str) -> Vec<String> {
+    open_buffered(filename)
+        .lines()
+        .map_while(Result::ok)
+        .collect()
+}
+
+#[allow(dead_code)]
 fn a1(part_two: bool) {
     let input = input_file("a1.1");
     let mut patterns = vec![
@@ -45,7 +54,36 @@ fn a1(part_two: bool) {
     println!("Total: {tot}");
 }
 
+fn a2() {
+    let input = input_file("a2.1");
+    let lines = read_all_lines(&input);
+    // only 12 red cubes, 13 green cubes, and 14 
+    let mut gametot = 0;
+    let mut powertot = 0;
+    for (line_no, line) in lines.iter().enumerate() {
+        let gameid = line_no + 1;
+        // Game 77: 10 blue, 2 red, 5 green; 5 green, 3 red, 12 blue; ...
+        let rgb = ["red", "green", "blue"].into_iter().map(|color| {
+            let re = Regex::new(&format!("(\\d+) {color}")).unwrap();
+            
+            re.captures_iter(line).map(|c| {
+                let (_, [count]) = c.extract();
+                count.parse::<u32>().unwrap()
+            }).max().unwrap_or(0)
+        }).collect::<Vec<_>>();
+
+        if rgb[0] <= 12 && rgb[1] <= 13 && rgb[2] <= 14 {
+            gametot += gameid;
+        }
+        powertot += (rgb[0] * rgb[1] * rgb[2]) as u64;
+
+    }
+    println!("Gametot: {gametot}");
+    println!("Powertot: {powertot}");
+}
+
 fn main() {
-    a1(false);
-    a1(true);
+    // a1(false);
+    // a1(true);
+    a2();
 }
