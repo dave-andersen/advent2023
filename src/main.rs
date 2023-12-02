14c0ca3ae2dff571ce9e7b05c43b42f11fe89954
Dave Andersen <dga@cs.cmu.edu>
2023-12-02 09:45:46 -0500

    Refactor a bit - add all_lines(filename) convenience function to make it easier to write these

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,17 @@ fn open_buffered(filename: &str) -> BufReader<File> {
     BufReader::new(File::open(filename).unwrap())
 }
 
-fn read_all_lines(filename: &str) -> Vec<String> {
-    open_buffered(filename)
-        .lines()
-        .map_while(Result::ok)
-        .collect()
+fn all_lines(filename: &str) -> impl Iterator<Item = String> {
+    open_buffered(filename).lines().map_while(Result::ok)
 }
 
 #[allow(dead_code)]
-fn a1(part_two: bool) {
+fn read_all_lines(filename: &str) -> Vec<String> {
+    all_lines(filename).collect()
+}
+
+#[allow(dead_code)]
+fn a1(part_two: bool) -> u32 {
     let input = input_file("a1.1");
     let mut patterns = vec![
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five",
@@ -36,9 +38,7 @@ fn a1(part_two: bool) {
 
     let ac = aho_corasick::AhoCorasick::new(patterns).unwrap();
 
-    let tot: u32 = open_buffered(&input)
-        .lines()
-        .map_while(Result::ok)
+    let tot: u32 = all_lines(&input)
         .map(|line| {
             let digits: Vec<_> = ac
                 .find_overlapping_iter(&line)
@@ -52,21 +52,24 @@ fn a1(part_two: bool) {
         })
         .sum();
     println!("Total: {tot}");
+    tot
 }
 
-fn a2() {
+// day 2:
+// only 12 red cubes, 13 green cubes, and 14 
+// input looks like:
+// Game 77: 10 blue, 2 red, 5 green; 5 green, 3 red, 12 blue; ...
+
+fn a2() -> (usize, u64) {
     let input = input_file("a2.1");
-    let lines = read_all_lines(&input);
-    // only 12 red cubes, 13 green cubes, and 14 
     let mut gametot = 0;
     let mut powertot = 0;
-    for (line_no, line) in lines.iter().enumerate() {
+    for (line_no, line) in all_lines(&input).enumerate() {
         let gameid = line_no + 1;
-        // Game 77: 10 blue, 2 red, 5 green; 5 green, 3 red, 12 blue; ...
         let rgb = ["red", "green", "blue"].into_iter().map(|color| {
             let re = Regex::new(&format!("(\\d+) {color}")).unwrap();
             
-            re.captures_iter(line).map(|c| {
+            re.captures_iter(&line).map(|c| {
                 let (_, [count]) = c.extract();
                 count.parse::<u32>().unwrap()
             }).max().unwrap_or(0)
@@ -80,10 +83,21 @@ fn a2() {
     }
     println!("Gametot: {gametot}");
     println!("Powertot: {powertot}");
+    (gametot, powertot)
 }
 
 fn main() {
-    // a1(false);
-    // a1(true);
+    //a1(false);
+    //a1(true);
     a2();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_all() {
+        assert_eq!(54968, super::a1(false));
+        assert_eq!(54094, super::a1(true));
+        assert_eq!((2795, 75561), super::a2());
+    }
 }
